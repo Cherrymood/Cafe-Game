f3fddaef9ae8e46e6c6108f601c788773678f368
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-29 13:01:08 -0700

    Added members

--- Cafe.cs
+++ Cafe.cs
@@ -1,4 +1,12 @@
 public class Cafe
 {
-    
+    private Customer _customer;
+    private Kitchen _kitchen;
+    private Workers _workers;
+    private string _order;
+
+    public Cafe(string order)
+    {
+        this._order = order;
+    }
 }
