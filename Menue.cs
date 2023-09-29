29251265133dbc449e0959598b9281bfb2a7437d
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-09-29 13:10:27 -0700

    Edit Munue enum to dict

---
+++ Menue.cs
@@ -0,0 +1,18 @@
+private class Menue
+{
+    { 
+        private Dictionary MenueKitchen = new Dictionary <string, int> {
+            {"Soup", 10},
+            {"Meat", 15},
+            {"Snaks", 2},
+            {"Salad", 5},
+            {"Pizza", 12}
+        };
+        private Dictionary MenueBar = new Dictionary <string, int> {
+            {"Water", 1},
+            {"Hot drink", 5},
+            {"Juice", 2},
+            {"Cocktail", 5},
+        };
+    }
+}
