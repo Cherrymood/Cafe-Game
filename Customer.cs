4496eaba2865cb6c48a6003c9151a12048823b71
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-10-03 12:37:10 -0700

    Edited privacy in Custom class

--- Customer.cs
+++ Customer.cs
@@ -3,25 +3,26 @@ class Customer
     private int _tips;
     private string _key;
     private Menue _menue;
-    private Menue _preparingTime;
 
     public Customer()
     {
         Console.WriteLine("Here is our menue.");
-        _menue = new Menue();
-        _preparingTime = new Menue();
+        this._menue = menue;
+        menue = new Menue();
+        this._key = key;
+        key = "";
         _tips =  1;
     } 
     
     public string MakeOrder()
     {
         Random rand = new Random();
-        List<TValue> keys = Enumerable.ToList(_menue.menue.Keys);
-        int size = _menue.menue.Count;
-        _key = keys[rand.Next(size)];
-        Console.WriteLine("Thank you, for your order. The waiting time is {0}, and the cost will be {1}", _preparingTime.menueTime(_key), _menue.menue(_key));
+        List<TValue> keys = Enumerable.ToList(menue.menuePrice.Keys);
+        int size = menue.menuePrice.Count;
+        key = keys[rand.Next(size)];
+        Console.WriteLine("Thank you, for your order. The waiting time is {0}, and the cost will be {1}", menue.menueTime(key), menue.menuePrice(key));
     
-        return _key;
+        return key;
     }
 
     public int Payment()
@@ -29,11 +30,11 @@ class Customer
         Random r = new Random(); // could i initialize it only in constructor and use it in all methods?
         int wait = r.Next(0, 12);
 
-        if (wait == _preparingTime.menueTime(_key))
+        if (wait == menue.menueTime(_key))
         {
             Console.WriteLine("Thank you. Here you are my money for my order.");
 
-            return _menue.menue(_key) + _tips;
+            return menue.menuePrice(key) + _tips;
         }
         else
         {
