750016ed94a9bd23f00f36c5ebb5d5356bd962e9
Valeriia <valeriia@Sergeys-MacBook-Pro.local>
2023-10-25 11:35:23 -0700

    Added more in Menue

--- Menue.cs
+++ Menue.cs
@@ -5,7 +5,8 @@ class Menu
 
     public Menu()
     {
-        this.menuPrice = new Dictionary<string, int> {
+        this.menuPrice = new Dictionary<string, int>
+        {
             {"Soup", 10},
             {"Meat", 15},
             {"Snacks", 2},
@@ -15,9 +16,21 @@ class Menu
             {"Hot drink", 5},
             {"Juice", 2},
             {"Cocktail", 5},
-            {"Nothing", 0}
+            {"Burger", 8},
+            {"Pasta", 11},
+            {"Ice Cream", 4},
+            {"Sushi", 20},
+            {"Steak", 18},
+            {"Sandwich", 6},
+            {"Fries", 3},
+            {"Pancakes", 7},
+            {"Coffee", 3},
+            {"Tea", 2},
+            {"Milkshake", 4},
+            {"Fish", 14}
         };
-        this.menuTime = new Dictionary<string, int> {
+        this.menuTime = new Dictionary<string, int>
+        {
             {"Soup", 5},
             {"Meat", 6},
             {"Snacks", 2},
@@ -27,7 +40,18 @@ class Menu
             {"Hot drink", 6},
             {"Juice", 6},
             {"Cocktail", 7},
-            {"Nothing", 0}
+            {"Burger", 8},
+            {"Pasta", 10},
+            {"Ice Cream", 4},
+            {"Sushi", 15},
+            {"Steak", 12},
+            {"Sandwich", 5},
+            {"Fries", 3},
+            {"Pancakes", 7},
+            {"Coffee", 3},
+            {"Tea", 2},
+            {"Milkshake", 4},
+            {"Fish", 14}
         };
     }
 }
