928cf44033a0658883e69a838b2ab2dd151f8e05
Valeriia <valeriia@Sergeys-MacBook-Pro.local>
2023-10-25 12:16:49 -0700

    Worked on game logic

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿class Program {
-  
-    
-    static public void Main(String[] args)
+﻿class Program
+{
+    static void Main(string[] args)
     {
-        Cafe Polyakova = new Cafe();
-        Polyakova.StartGame();
+        Cafe cafe = new Cafe();
+
+        while (cafe.Level <= 3) // You can change the number of levels as needed
+        {
+            Console.WriteLine($"Level {cafe.Level}");
+            cafe.StartGame();
+        }
+
+        Console.WriteLine("Congratulations! You've completed all levels.");
     }
 }
