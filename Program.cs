aaa7e47d85db3bdaf21464d43d85c05ebf55c70d
Valeriia <121061578+Cherrymood@users.noreply.github.com>
2023-11-16 10:55:09 -0800

    Update Program.cs (#2)

--- Program.cs
+++ Program.cs
@@ -3,14 +3,6 @@
     static void Main(string[] args)
     {
         Cafe cafe = new Cafe();
-
-        while (cafe.Level <= 2) // You can change the number of levels as needed
-        {
-            Console.WriteLine($"Level {cafe.Level}");
-            cafe.StartGame();
-            cafe.IncreaseLevel();
-        }
-
-        Console.WriteLine("Congratulations! You've completed all levels.");
+        cafe.StartGame();
     }
 }
