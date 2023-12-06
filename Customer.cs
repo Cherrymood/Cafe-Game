8a33ecfdb49e2f341f16156ac6477ac90d8ba0ff
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2023-12-05 16:36:51 -0800

    Added modifications to Customer

--- Customer.cs
+++ Customer.cs
@@ -1,56 +1,28 @@
 
+using System.Reflection.Metadata.Ecma335;
+
 class Customer{
 
-    private int _tips;
-    private List<string> _keys;
     private Kitchen _menu;
-    private Random _random;
-    private int _totalCost;
 
     public Customer()
     {
         Console.WriteLine("Here is our menu.");
+        
         _menu = new Kitchen();
-        _keys = new List<string>();
-        _tips = 1;
-        _random = new Random();
-        _totalCost = 0;
     }
 
-    public int MakeOrder()
+    public string MakeOrder()
     {
-        _totalCost = 0; // Reset totalCost for each customer
-        
-        while (true)
-        {
-            Console.WriteLine("Menu:");
-            foreach (var item in _menu.menuPrice)
-            {
-                Console.WriteLine($"{item.Key}: ${item.Value}");
-            }
+        Console.Write("Enter your order (or 'q' to quit): ");
 
-            Console.Write("Enter your order (or 'q' to quit): ");
-            string order = Console.ReadLine();
+        string order = Console.ReadLine();
 
-            if (order.ToLower() == "q")
-            {
-                Console.WriteLine($"Your total cost is: ${_totalCost}");
-                break;
-            }
+        _menu.OrderDish(order);
 
-            if (_menu.menuPrice.ContainsKey(order, StringComparer.OrdinalIgnoreCase))
-            {
-                _totalCost += _menu.menuPrice[order];
-                _keys.Add(order);
-                Console.WriteLine($"Added {order} to your order. Total cost: ${_totalCost}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid item. Please choose from the menu.");
-            }
-        }
+        return order;
 
-        return _totalCost; // Return the total cost of the order
     }
 }
+        
 
