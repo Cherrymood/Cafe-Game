ccc5840aeb36261e61b256fc20da05c5adca537b
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-02-23 14:14:15 -0800

    Handled null NullReferenceException

--- SQL Server/Menue/Dish.cs
+++ SQL Server/Menue/Dish.cs
@@ -1,8 +1,9 @@
 using System;
+
 public class Dish
-    {
-        public int DishID { get; set; }
-        public string DishName { get; set; }
-        public int Price { get; set; }
-        public string DishDescription { get; set; }
-    }
+{
+    public int DishID { get; set; }
+    public string? DishName { get; set; }
+    public int? Price { get; set; }
+    public string? DishDescription { get; set; }
+}
